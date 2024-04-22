34301a9a17e2cb01cd859f1188002a50fa499c30
0xrakesh <rakeshhacks354@gmail.com>
2024-04-22 14:17:14 +0530

    Add the backend url on the code editor component

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -11,7 +11,7 @@ const CodeEditor = () => {
     const [output, setOutput] = useRecoilState(outputValue)
     const [transmit, setTransmit] = useState(false);
     const [width, setWidth] = useState('');
-    const socket = io(`ws://${REACT_APP_COMPILER_URL}`);
+    const socket = io(`ws://${process.env.REACT_APP_COMPILER_URL}`);
 
     const handleCodeInput = (event) => {
         setCode(event)
