5bce31e1fd1ce4389f452c5e4704ab68c808c694
0xrakesh <rakeshhacks354@gmail.com>
2024-04-30 14:53:44 +0530

    Change the protocol of socket as https

--- src/hooks/socket.js
+++ src/hooks/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-let URL =  `http://${process.env.REACT_APP_SOCKET_URL}`;
+let URL =  `https://${process.env.REACT_APP_SOCKET_URL}`;
 
 export const socket = io(URL,{
     autoConnect:false
