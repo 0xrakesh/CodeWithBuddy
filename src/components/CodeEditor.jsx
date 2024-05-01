4353423c4520eedb8591b8c3b0fcdc8e4ff2dda4
0xrakesh <rakeshhacks354@gmail.com>
2024-05-01 20:10:58 +0530

    Open feature added

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -19,6 +19,8 @@ const CodeEditor = () => {
     const [userMode,setUserMode] = useRecoilState(mode);
     const [transmit, setTransmit] = useState(false);
     const [roomName, setRoomName] = useState('');
+    const [ lang, setLang] = useState('python');
+    const [file,setFile] = useState('')
 
     const editorRef = useRef(null);
     const [copy,setCopy] = useState("Click to copy")
@@ -76,27 +78,30 @@ const CodeEditor = () => {
     const RunCode = () => {
         let url = process.env.REACT_APP_COMPILER_URL;
         let data = {
-            language: 'python',
+            language: lang,
             code:codes,
             input: [[]]
         }
         const fetchOutput = async() => {
+            console.log(data);
             const response = await fetch(url+"/playground/run",{
                 method:"POST",
                 headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`,
                     "Content-Type":"application/json"
                 },
                 body: JSON.stringify(data)
             })
             const result = await response.json();
+            console.log(result)
 
             let tmp = result.output
-            if(tmp[0].status === "Compilation error") {
+            if(tmp[0]?.status === "Compilation error") {
                 setOutput(tmp[0].status)
                 setError((tmp[0].error))
             }
             else {
-                tmp = tmp[0].split('\n')
+                tmp = tmp[0]?.split('\n')
                 setOutput(tmp);
             }
         }
@@ -126,12 +131,132 @@ const CodeEditor = () => {
         setRoomName(event.target.value)
     }
 
+    const handleKeyboard = (event) => {
+        if(event.ctrlKey && event.key === "r") {
+            RunCode() 
+            event.preventDefault();
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeyboard);
+        return () => document.removeEventListener('keydown', handleKeyboard);
+    })
+
+    const handleSave = async () => {
+        let url = process.env.REACT_APP_COMPILER_URL;
+        let data = {
+            code:codes,
+            name:file
+        }
+        console.log(file)
+        if(file.length !==0 && file !== "") {
+            let response = await fetch(`${url}/files/save`,{
+                method:"POST",
+                headers:{
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`,
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify(data)
+            });
+
+            try {
+                let result = await response.json();
+                alert(result.status)
+            }
+            catch {
+                console.log(response);
+                alert(response)
+            }
+        }
+        else {
+            alert("Enter the file name.")
+        }
+    }
+
+    const handleFileInput = (event) => {
+        setFile(event.target.value);
+    }
+
+    // Modal
+    const Modal = ({onClose,onCode}) => {
+
+        const [fileNames,setFiles] = useState([]) 
+
+        useEffect(() => {
+            const files = async () => {
+                let url = process.env.REACT_APP_COMPILER_URL;
+                try {
+                    let response = await fetch(`${url}/files/get`,{
+                        method:"GET",
+                        headers: {
+                            "Content-Type":"application/json",
+                            "Authorization":`Bearer ${localStorage.getItem("token")}`
+                        }
+                    });
+    
+                    let data = await response.json();
+                    setFiles(data.files);
+                    console.log(data.files);
+    
+                }
+                catch(err) {
+                    alert("Something went wrong")
+                }
+            }
+
+            files();
+        },[])
+
+
+        const handleCodeOpen = (code) => {
+            onCode(code);
+            onClose((prev) => !prev)
+        }
+
+        return (
+            <Fragment>
+                <div className='fixed top-24 border-2 border-black z-50 mx-48 text-black w-4/5 bg-violet-300/95 h-96'>
+                    <ol className='text-white text-center'>
+                        {fileNames ? 
+                            fileNames.map((file,index) => (
+                                <li className='border-b-[1px] py-4 hover:cursor-pointer' onClick={() => handleCodeOpen(file.code)} key={index}>{file.name}</li>
+                            ))
+                            :
+                            <>Nothing saved</>
+                        }
+                    </ol>
+                    <button className='bg-transparent text-white font-bold absolute right-4 bottom-4' onClick={() => onClose((prev) => !prev)}>Close</button>
+                </div>
+            </Fragment>
+        )
+    }
+
+    const [toggle, setToggle] = useState(false)
+
     return (
         <Fragment>
+            <div className='mt-10 flex gap-5 px-5 items-center'>
+                <p>Filename</p>
+                <input type="text" value={file} placeholder='Enter the filename' onChange={handleFileInput} className='bg-white/20 px-4 py-2 rounded'/>
+                {
+                        userMode === "collab" ? 
+                            <>
+                                <div className='bg-white text-black rounded-md px-2 font-bold py-3'>{roomName}</div>
+                                <p className='bg-primary/50 rounded-md px-2 text-sm text-center hover:cursor-pointer hover:font-bold py-4' onClick={handleCopy}>{copy}</p>
+                            </> : userMode === "join" ?
+                            <>
+                                <input type='name' placeholder='Room ID' className='w-28 rounded-md px-4 py-2 text-black' onChange={handleRoomInput}/>
+                                <button className='bg-green-400 text-black text-center px-4 py-2 rounded-md font-medium' onClick={handleJoin} >{join}</button>
+                            </> : <></>
+                }
+                <button onClick={() => setToggle((prev) => !prev)}>Open a file</button>
+                {toggle && <Modal onClose={setToggle} onCode={setCode}/>}
+            </div>
             <div className='flex p-5 gap-5 my-5 bg-violet-300 h-fit'>
                 <Editor
                     height="50vh"
-                    defaultLanguage='python'
+                    defaultLanguage={lang}
                     defaultValue='console.log("Welcome to code with buddy.")'
                     onMount={focusOnMount}
                     theme={NightOwl}
@@ -139,23 +264,20 @@ const CodeEditor = () => {
                     onChange={handleCodeInput}
                 />
                 <div className="buttons flex gap-3 flex-col">
-                    <button className='bg-white text-black shadow-lg shadow-indigo-500/40 px-4 py-2 rounded-md' onClick={ClearCode}>Clear Code</button>
+                    <select className='bg-violet-300 border-[1px] rounded-md px-2 py-2 text-white' onChange={(e) => setLang(e.target.value)}>
+                        <option value="python">Python</option>
+                        <option value="c">C</option>
+                        <option value="javascript">Javascript</option>
+                    </select>
+                    <button className='bg-white text-black shadow-lg shadow-indigo-500/40 px-4 py-2 rounded-md' onClick={ClearCode}>Clear</button>
                     <button className='bg-green-300 shadow-lg text-green-800 shadow-green-400/40 px-4 py-2 rounded-md' onClick={RunCode}>Run Code</button>
+                    <button className='bg-teal-400 shadow-lg text-black shadow-teal-400/40 px-4 py-2 rounded-md' onClick={handleSave}>Save</button>
+                    
                     { userMode === "solo" || userMode === "" ? 
                         <></> :
                         <button className='bg-purple-500 text-white shadow-lg shadow-purple-400/40 px-4 py-2 rounded-md' onClick={handleTransmission}>{transmit ? "Collab" : "Dev Mode"}</button>
                     }
-                    {
-                        userMode === "collab" ? 
-                            <>
-                                <div className='bg-white text-black rounded-md px-2 font-bold py-4'>{roomName}</div>
-                                <p className='bg-primary/50 rounded-md px-2 text-sm text-center hover:cursor-pointer hover:font-bold py-4' onClick={handleCopy}>{copy}</p>
-                            </> : userMode === "join" ?
-                            <>
-                                <input type='name' placeholder='Room ID' className='w-28 rounded-md px-4 py-2 text-black' onChange={handleRoomInput}/>
-                                <button className='bg-green-400 text-black text-center px-4 py-2 rounded-md font-medium' onClick={handleJoin} >{join}</button>
-                            </> : <></>
-                    }
+
                 </div>
             </div>
         </Fragment>
